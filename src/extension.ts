e6fc6c77e4cb755401fadbfefe0c41a83a6363d8
Jose Francisco Gonzalez <jfgs1609@gmail.com>
2025-06-05 00:55:04 +0200

    Initial release

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
 import { ExtensionContext, StatusBarAlignment, StatusBarItem, window, workspace, commands, QuickPickOptions, TabInputText, Uri, WorkspaceFoldersChangeEvent } from 'vscode';
 import { FolderView, Tab } from './interfaces';
 
@@ -25,6 +21,13 @@ export function activate(context: ExtensionContext) {
 	// Detect changes in workspace folders
 	context.subscriptions.push(workspace.onDidChangeWorkspaceFolders((event) => changedWorkspaceFolders(event, context)));
 
+	// Detect changes in settings
+	context.subscriptions.push(workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration("workspaceViews.statusColor")) {
+            updateStatusBarItem();
+        }
+    }));
+
 	updateStatusBarItem();
 	loadActualFolderView(context);
 }
@@ -36,12 +39,12 @@ function showSelectFolder(context: ExtensionContext) {
 	if(items.length) {
 		window.showQuickPick(items, options).then(selectedFolder => {
 			if(selectedFolder){
-				window.showInformationMessage(selectedFolder);
 				saveActualFolderView(context);
 				actualFolderView = selectedFolder;
 				context.workspaceState.update('last-folder-view', actualFolderView);
 				loadActualFolderView(context);
-			}
+				updateStatusBarItem();
+			 }
 		});
 	}
 }
@@ -59,7 +62,7 @@ function saveActualFolderView(context: ExtensionContext) {
 	let folderView: FolderView | undefined = foldersViews.find(folderView => folderView.name === actualFolderView);
 
 	if(!folderView) {
-		folderView = {name: actualFolderView, tabs: getOpenTabs()};
+		folderView = {name: actualFolderView, path: getWorkspaceFolderUri(actualFolderView), tabs: getOpenTabs()};
 		foldersViews.push(folderView);
 	} else {
 		folderView.tabs = getOpenTabs();
@@ -68,12 +71,16 @@ function saveActualFolderView(context: ExtensionContext) {
 	context.workspaceState.update('folders-views', JSON.stringify(foldersViews));
 }
 
+function getWorkspaceFolderUri(folderName: string): string {
+	return workspace.workspaceFolders?.find(workspaceFolder => workspaceFolder.name === folderName)?.uri.path || '';
+}
+
 function getOpenTabs(): Tab[] {
 	return window.tabGroups.all.flatMap(group =>
 		group.tabs
 			.map(tab => {
 				if (tab.input instanceof TabInputText) {
-					return { label: tab.label, uri: (tab.input as TabInputText).uri.path } as Tab;
+					return { label: tab.label, path: (tab.input as TabInputText).uri.path } as Tab;
 				}
 				return undefined;
 			})
@@ -96,15 +103,29 @@ function getFoldersViews(context: ExtensionContext): FolderView[] {
 function loadActualFolderView(context: ExtensionContext) {
 	const folderView: FolderView = getFoldersViews(context).find(folderView => folderView.name === actualFolderView)!;
 
-	commands.executeCommand('workbench.action.closeAllEditors');
+	executeLoadCommands(folderView);
 
 	if (folderView) {
 		folderView.tabs.forEach(tab => {
-			const uri = Uri.file(tab.uri);
+			const uri = Uri.file(tab.path);
 			commands.executeCommand('vscode.open', uri);
 		});
 	}
 }
+
+function executeLoadCommands(folderView: FolderView) {
+	// Close all tabs
+	commands.executeCommand('workbench.action.closeAllEditors');
+
+	// TO-DO: Execute if config is true
+	if (folderView) {
+		commands.executeCommand('workbench.files.action.collapseExplorerFolders');
+		commands.executeCommand("revealInExplorer", Uri.file(folderView.path));
+		commands.executeCommand('list.expand');
+		// commands.executeCommand('workbench.action.revealFileInExplorer');
+	}
+}
+
 
 function changedWorkspaceFolders(event: WorkspaceFoldersChangeEvent, context: ExtensionContext) {
 	if(event.removed.length) {
@@ -131,7 +152,7 @@ function updateStatusBarItem() {
 	if (getWorkspaceFolders().length > 1) {
 		const multiRootConfigForResource = workspace.getConfiguration('workspaceViews');
 
-		statusBarItem.color = multiRootConfigForResource.get('statusColor') || '#F00';
+		statusBarItem.color = multiRootConfigForResource.get('statusColor');
 		statusBarItem.text = `$(file-submodule) ${actualFolderView}`;
 		statusBarItem.show();
 	} else {
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,10 @@
 export interface FolderView {
 	name: string;
+	path: string;
 	tabs: Tab[];
 }
 
 export interface Tab {
 	label: string;
-	uri: string;
+	path: string;
 }
